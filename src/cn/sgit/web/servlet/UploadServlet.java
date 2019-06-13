d66214687ee7c844252f62cd9288d9024f8015d1
814792647 <814792647@qq.com>
2019-06-13 15:04:11 +0800

    demo

--- src/cn/sgit/web/servlet/UploadServlet.java
+++ src/cn/sgit/web/servlet/UploadServlet.java
@@ -25,77 +25,28 @@ public class UploadServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		try {
-		response.setContentType("text/html;charset=utf-8");
-		DiskFileItemFactory factory=new DiskFileItemFactory();
-		File f =new File("F:\\apache-tomcat-7.0.92\\www\\cache\\");
-		if(!f.exists()){
-			f.mkdirs();
-		}
-		//设置文件缓存路径
-		factory.setRepository(f);
-		//创建ServletFileUpload对象
-		ServletFileUpload fileupload=new ServletFileUpload(factory);
-		//设置字符编码
-		fileupload.setHeaderEncoding("utf-8");
-		//解析request,得到fileItem对象
 		
-			List<FileItem> fileitems=fileupload.parseRequest(request);
-			PrintWriter write=response.getWriter();
-			//遍历集合
-			for(FileItem  fileitem :fileitems){
-				//判断是否为普通字段
-				if(fileitem.isFormField()){
-					//获得字段名
-					String name =fileitem.getFieldName();
-					if(name.equals(name)){
-						if(!fileitem.getString().equals("")){
-							String value =fileitem.getString("utf-8");
-							write.print("上传者"+value+"<br/>");
-						}
-						}
-					}else{
-						//获取上传的文件名
-						String  filename= fileitem.getName();
-						//处理上传文件
-						if(fileitem !=null&& !fileitem.equals("")){
-							write.print("上传的文件名称是："+filename+"<br/>");
-							//截取出文件名
-							filename =filename.substring(filename.lastIndexOf("\\")+1);
-							//文件名需要唯一
-							filename =UUID.randomUUID().toString()+"_"+filename;
-							//在服务器创建同名文件
-							String webpath="/cache/";
-							String filepath =getServletContext().getRealPath(webpath+filename);
-							//创建文件
-							File file=new File(filepath);
-							file.getParentFile().mkdirs();
-							file.createNewFile();
-							InputStream in=fileitem.getInputStream();
-							FileOutputStream out=new FileOutputStream(file);
-							byte[]buffer= new byte[1024];//每次读取1个字节
-							int len;
-							//开始读取上传文件的字节，并输出到服务期端的上传文件输出流中
-							while((len=in.read(buffer))>0){
-								out.write(buffer,0,len);
-								//关闭流
-								in.close();
-								out.close();
-								//删除临时文件
-								fileitem.delete();
-								write.print("上传文件成功！<br/>");						
-							}
-						}
-					}
-				}
-		} catch (FileUploadException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException {
-		doGet(request,response);
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter write = response.getWriter();
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload sfu = new ServletFileUpload(factory);
+		try {
+			List<FileItem> fileItemList = sfu.parseRequest(request);
+			for(FileItem  fileitem :fileItemList){
+				if(!fileitem.isFormField()){
+					System.out.println("upload: " + fileitem.getName());
+					File file = new File("F:\\apache-tomcat-7.0.92\\www\\cache\\" + fileitem.getName());
+					fileitem.write(file);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect("/OA/Base_html/personnel_management.html"); 
 	}
 
 }
